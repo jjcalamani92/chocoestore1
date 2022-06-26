5df8bc053b5861878aed506a84add88ec1aa1dc6
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-06-26 01:25:31 +0000

    v

--- pages/admin/marks/index.tsx
+++ pages/admin/marks/index.tsx
@@ -6,10 +6,11 @@ import { S } from "../../../src/gql/siteQuery";
 import { GetStaticProps } from "next";
 import { Category, IMark, ISeo, Item, Section, Site } from "../../../src/interfaces";
 import { FormSite } from "../../../components/Layout/admin/FormSite";
-import { LayoutCategoryListAdmin, LayoutMarkListAdmin } from "../../../components/Components";
+import { LayoutCategoryListAdmin, LayoutMarkListAdmin, Spinner04 } from "../../../components/Components";
 import { TableCategory } from "../../../components/Components/table/TableCategory";
 import { MARKS } from "../../../src/gql/markQuery";
 import { TableMark } from "../../../components/Components/table/TableMark";
+import { useQuery } from "@apollo/client";
 
 
 interface Props {
@@ -18,13 +19,20 @@ interface Props {
 }
 const AdminPages:FC<Props> = ({seo, markAll}) => {
   // console.log(markAll)
+  const { loading, error, data, fetchMore } = useQuery(MARKS, {
+		variables: { site: process.env.API_SITE },  
+		fetchPolicy: 'network-only',
+		onCompleted: () => console.log('called'),
+});
+if (loading) return <Spinner04 />;
+  console.log(data)
 	return (
 		<>
 			<LayoutAdmin>
-        <TableMark markAll={markAll} />
+        <TableMark markAll={data.markAll} />
 				{/* <TableCategory categories={site.categories} /> */}
 				{/* <LayoutCategoryListAdmin data={site.categories}/> */}
-        <LayoutMarkListAdmin marks={markAll} />
+        <LayoutMarkListAdmin marks={data.markAll} />
 				{/* <FormSite site={site} /> */}
 			</LayoutAdmin>
 		</>
